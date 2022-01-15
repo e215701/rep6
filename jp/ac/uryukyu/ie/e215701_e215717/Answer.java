f2a68a9cfc97f138030be274a859c82066009ac4
taito takara <e215717@ie.u-ryukyu.ac.jp>
2022-01-15 15:50:55 +0900

    Answerクラスのgetterメソッド追加

--- jp/ac/uryukyu/ie/e215701_e215717/Answer.java
+++ jp/ac/uryukyu/ie/e215701_e215717/Answer.java
@@ -1,5 +1,25 @@
 package jp.ac.uryukyu.ie.e215701_e215717;
 
-public class Answer {
+class Answer {
+    private int Touma;
+    private int Akamine;
+    private int Wada;
     
+    Answer(int Touma, int Akamine, int Wada){
+        this.Touma = Touma;
+        this.Akamine = Akamine;
+        this.Wada = Wada;
+    }
+
+    int getTouma(){
+        return Touma;
+    }
+
+    int getAkamine(){
+        return Akamine;
+    }
+
+    int getWada(){
+        return Wada;
+    }
 }
