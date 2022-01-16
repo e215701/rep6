7cbcb76a61f1f39671ea6abd9d185728b1c710c6
Minami ASATO <e215701@ie.u-ryukyu.ac.jp>
2022-01-16 17:49:29 +0900

    いらないものを消去（これで完成）

--- jp/ac/uryukyu/ie/e215701_e215717/JudgePerson.java
+++ jp/ac/uryukyu/ie/e215701_e215717/JudgePerson.java
@@ -12,52 +12,21 @@ public class JudgePerson{
     */
     
     public static void Judge(String name){
-    /*Touma,Akamine,Wadaそれぞれ最も大きい時で条件を分岐させる */
-    if (Touma > Akamine && Touma > Wada){
-        System.out.println(name + "が思い描いているのはトウマ教授ですね");
+        /*Touma,Akamine,Wadaそれぞれ最も大きい時で条件を分岐させる */
+        if (Touma > Akamine && Touma > Wada){
+            System.out.println(name + "が思い描いているのはトウマ教授ですね");
+        }
+        //Toumaが最も大きい値の時の出力↑       
+        else if (Akamine > Touma && Akamine > Wada){
+            System.out.println(name + "が思い描いているのはアカミネ教授ですね");
+        }
+        //Akamineが最も大きい値の時の出力↑
+        else if (Wada > Touma && Wada > Akamine){
+            System.out.println(name + "が思い描いているのはワダ教授ですね");
+        }
+        //Wadaが最も大きい値の時の出力↑
+        else {System.out.println("判別ができませんでした。もう一度お試しください。");
+        }
+        //それ以外の時の出力↑
     }
-    //Toumaが最も大きい値の時の出力↑       
-    else if (Akamine > Touma && Akamine > Wada){
-        System.out.println(name + "が思い描いているのはアカミネ教授ですね");
-    }
-    //Akamineが最も大きい値の時の出力↑
-    else if (Wada > Touma && Wada > Akamine){
-        System.out.println(name + "が思い描いているのはワダ教授ですね");
-    }
-    //Wadaが最も大きい値の時の出力↑
-    else {System.out.println("判別ができませんでした。もう一度お試しください。");
-    }
-    //それ以外の時の出力↑
-}
-
-
-    /*InfoTouma(int Touma , int Akamine, int Wada){
-        this.Touma = Touma;
-        this.Akamine = Akamine;
-        this.Wada = Wada; 
-    }*/
-
-    /*public void setTouma(int Touma){
-        this.Touma = Touma;
-    }
-
-    public  int getTouma(){
-        return this.Touma;
-    }
-
-    public void setWada(int WadaPoint){
-        this.Wada = WadaPoint;
-    }
-
-    public int getWada(){
-        return Wada;
-    }
-
-    public void setAkamine(int AkaminePoint){
-        this.Akamine = AkaminePoint;
-    }
-
-    public int getAkamine(){
-        return Akamine;
-    }*/
 }
