237e76eb9289d5809cec655df42a9e83be20c04a
taito takara <e215717@ie.u-ryukyu.ac.jp>
2022-01-16 09:50:14 +0900

    カプセル化エラー未処理

---
+++ jp/ac/uryukyu/ie/e215701_e215717/Judge.java
@@ -0,0 +1,33 @@
+package jp.ac.uryukyu.ie.e215701_e215717;
+
+class Judge extends InfoTouma {
+    Judge(int Touma, int Akamine, int Wada) {
+        super(Touma, Akamine, Wada);
+    }
+
+     int Touma  = 1;  int Akamine = 2;  int Wada =3;
+
+
+    
+    public  void JudgePerson(int Touma, int Akamine, int Wada){
+        if (Touma > Akamine && Touma > Wada){
+            System.out.println("あなたが思い描いているのはトウマ教授ですね");
+        }       
+        else if (Akamine > Touma && Akamine > Wada){
+            System.out.println("あなたが思い描いているのはアカミネ教授ですね");
+        }
+        else if (Wada > Touma && Wada > Akamine){
+            System.out.println("あなたが思い描いているのはワダ教授ですね");
+        }
+        else {System.out.println("else");
+        }
+   
+    }
+
+    public  static void main(String[] args){
+        Judge person = new Judge(1,2,3);
+        System.out.println(person);
+    }
+
+    
+}
